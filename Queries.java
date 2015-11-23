5a0fcca651159547682b33c5d18ae23acb080f93
ChuckRozsa <chuckeroz@gmail.com>
2015-11-23 14:41:00 -0700

    Added logic of getting queries from user

--- Queries.java
+++ Queries.java
@@ -9,10 +9,35 @@ public class Queries
       System.out.print("Please enter a query: ");
       String queryString = input.nextLine();
       Scanner queryInput = new Scanner(queryString);
-      
+      //Creates a ArrayList of queries all with 0 whitespace
       while (queryInput.hasNext())
       {
-      queries.add(queryInput.next());
+        String curr = queryInput.next();
+        if (curr.equals("pprice") || curr.equals("rscore") || curr.equals("rdate"))
+        {
+          String curr2 = queryInput.next();
+          if (curr2.equals(">") || curr2.equals("<"))
+          {
+            queries.add(curr + curr2 + queryInput.next());
+          } else if (curr2.contains(">")|| curr2.contains("<"))
+          {
+            queries.add(curr + curr2);
+          }
+        } else if (curr.contains("pprice") || curr.contains("rscore") || curr.contains("rdate"))
+        {
+          if ( curr.matches(".*\\d+.*") )
+          {
+            queries.add(curr);
+          } else
+          {
+            queries.add( curr + queryInput.next());
+          }
+        } else
+        {
+          queries.add(curr);
+        }
+
+            
       }
       
       for ( int i =0; i < queries.size(); i++)
