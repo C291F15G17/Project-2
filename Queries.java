1fd783cf447bb11a048a8cf0b25efafa4d1e07d3
cjresler <resler@ualberta.ca>
2015-11-24 13:07:02 -0700

    Update Queries.java

--- Queries.java
+++ Queries.java
@@ -142,7 +142,7 @@ public class Queries
             System.out.println(data.getData());
             while (cursor.getNext(key, data, LockMode.DEFAULT) == OperationStatus.SUCCESS)
             {
-              System.out.println(data.getData());
+              System.out.println(key.toString());
               set.add(new String(data.getData()));
             }
           }
